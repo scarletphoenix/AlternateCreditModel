cc60b3f029923ff435a89eeb2e275e9afe47c10b
Rashmi Agrawal <rashmi.agrawal@nirafinance.com>
2019-12-08 10:21:21 +0530

    add userid as path param for getting user score

--- app_routes.py
+++ app_routes.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 from flask import Blueprint, request
 import json
-from fetch_data import fetch_all_users_lifestyle, fetch_all_users_repayment
+from fetch_data import fetch_user_score
 from life_style_assessment_matrix.rule_engine import life_style_assesment_matrix_engine
 from repayment_capacity.rule_engine import repayment_capacity_engine
 urls_blueprint = Blueprint('urls', __name__,)
 
-# @urls_blueprint.route('/lifestyle/list')
-# def lifestyle_data_all_users():
-#     return fetch_all_users_lifestyle()
-
-@urls_blueprint.route('/repayment/list')
-def repayment_data_all_users():
-    return fetch_all_users_repayment()
+@urls_blueprint.route('/creditscore/user/<id>')
+def repayment_data_all_users(id):
+    return fetch_user_score(id)
 
 @urls_blueprint.route('/lifestyle/user/create',methods=['POST'])
 def create_new_user_lifestyle():
