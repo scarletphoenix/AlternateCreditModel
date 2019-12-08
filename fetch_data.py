399641fa3cffac1e30e67f33a6a7ec560691acb6
Rashmi Agrawal <rashmi.agrawal@nirafinance.com>
2019-12-08 07:04:14 +0530

    file name correction

--- fetch_data.py
+++ fetch_data.py
@@ -7,6 +7,6 @@ def fetch_all_users_lifestyle():
       return (json.dumps(data, indent=4, sort_keys=True))
 
 def fetch_all_users_repayment():
-  with open('./repayment_capacity/repayment_capacity_user_data.json', 'r') as json_file:
+  with open('./repayment_capacity/repayment_user_data.json', 'r') as json_file:
       data = json.load(json_file)   
       return (json.dumps(data, indent=4, sort_keys=True))
